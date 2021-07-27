3d7d573bae6e9e73cbcf0da42462d6bc2a870a63
Melgaard <marcusmelgaardj@gmail.com>
2021-07-27 19:44:20 +0200

    members.js .data

--- src/services/members.js
+++ src/services/members.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 export default {
 	async getAll() {
-		const res = await axios.get(`https://run.mocky.io/v3/90cfbed9-924c-423f-8e4b-dde6a6be438b`)
-		return res;
+		const res = await axios.get(`https://run.mocky.io/v3/90cfbed9-924c-423f-8e4b-dde6a6be438b`);
+		return res.data;
 	},
 	async signup(member) {
-		const res = await axios.post(`https://run.mocky.io/v3/029be3b9-c4d0-42c9-b348-dc2b575e3312`, member)
-		return res;
+		const res = await axios.post(`https://run.mocky.io/v3/029be3b9-c4d0-42c9-b348-dc2b575e3312`, member);
+		return res.data;
 	}
 }
