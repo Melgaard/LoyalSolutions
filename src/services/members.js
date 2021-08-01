faf466825e7e5dbdebd3a9ddb00b8248f6234cf3
Melgaard <marcusmelgaardj@gmail.com>
2021-08-01 12:38:35 +0200

    table

--- src/services/members.js
+++ src/services/members.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 export default {
 	async getAll() {
 		const res = await axios.get(`https://run.mocky.io/v3/90cfbed9-924c-423f-8e4b-dde6a6be438b`);
-		return res.data;
+		return res.data.members;
 	},
 	async signup(member) {
 		const res = await axios.post(`https://run.mocky.io/v3/029be3b9-c4d0-42c9-b348-dc2b575e3312`, member);
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="header">
-    {{ members }}
-  </div>
+<div class="header">
+	<table>
+		<tr class="headerRow">
+			<th>Name</th>
+			<th>Lastname</th>
+			<th>Email</th>
+		</tr>
+		<tr v-for="member in displayedMembers" :key="member.email" class="tableRow">
+			<th>{{member.name}}</th>
+			<th>{{member.last_name}}</th>
+			<th>{{member.email}}</th>
+		</tr>
+	</table>
+	
+</div>
 </template>
 
 <script>
@@ -14,16 +26,38 @@ export default {
 	},
 	data() {
 		return {
-			members: []
-	}	
+			members: [],
+			currentPage: 1,
+			rowsPerPage: 10
+		}
 	},
 	async created() {
 		this.members = await Members.getAll();
+	},
+	computed: {
+		displayedMembers() {
+			return this.members.slice(this.currentPage * this.rowsPerPage, (this.currentPage + 1) * this.rowsPerPage)
+		}
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+.header {
+	display: flex;
+	justify-content: center;
+}
+.headerRow th {
+	font-size: 20px;
+	border-bottom: 1px solid black;
+}
+.tableRow {
+	text-align: left;
+
+	th {
+		padding: 5px 5px 5px 10px;
+	}
+}
 
 </style>
